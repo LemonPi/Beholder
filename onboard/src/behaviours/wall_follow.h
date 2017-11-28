e9ebebfa91d4579a712c849503b32b49f7e13083
Lemonpi <johnson9510@hotmail.com>
2017-11-28 01:21:22 -0500

    Optionally use both walls to follow
    Use whichever wall is available, including when both are.

--- onboard/src/behaviours/wall_follow.h
+++ onboard/src/behaviours/wall_follow.h
@@ -8,15 +8,15 @@ class WallFollow {
     enum State { INACTIVE, FOLLOWING, PRE_TURN, TURNING, NUM_STATES };
 
     // gains for wall follow controller, scaled for 1s
-    static constexpr auto WALL_KP = 1.5;
+    static constexpr auto WALL_KP = 1.0;
     static constexpr auto WALL_KD = 0.2;
     static constexpr auto WALL_KI = 0;
 
   public:
     static constexpr auto MAX_FOLLOW_DIST_MM = 250;
     // used for constant forward velocity
-    static constexpr auto WALL_FWD_PWM = 210;
-    static constexpr auto DESIRED_WALL_DIST_MM = 95;
+    static constexpr auto WALL_FWD_PWM = 170;
+    static constexpr auto DESIRED_WALL_DIST_MM = 80;
 
     WallFollow();
 
